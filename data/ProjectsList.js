d9b7f3bcb522b00ac4c65d0b6958a754161b311a
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-05-03 13:12:08 +0600

    update

---
+++ data/ProjectsList.js
@@ -0,0 +1,71 @@
+// import images
+import FP1 from '../public/img/fp-1.jpg';
+import FP2 from '../public/img/fp-2.jpg';
+import FP3 from '../public/img/fp-3.jpg';
+import FP4 from '../public/img/fp-4.jpg';
+
+
+const Projects = [
+    {
+        id: 1,
+        image: FP1,
+        category: "Website"
+    },
+    {
+        id: 2,
+        image: FP2,
+        category: "Application"
+    },
+    {
+        id: 3,
+        image: FP3,
+        category: "Branding"
+    },
+    {
+        id: 4,
+        image: FP4,
+        category: "UI/UX"
+    },
+    {
+        id: 5,
+        image: FP1,
+        category: "Website"
+    },
+    {
+        id: 6,
+        image: FP2,
+        category: "Application"
+    },
+    {
+        id: 7,
+        image: FP3,
+        category: "Branding"
+    },
+    {
+        id: 8,
+        image: FP4,
+        category: "UI/UX"
+    },
+    {
+        id: 9,
+        image: FP1,
+        category: "Website"
+    },
+    {
+        id: 10,
+        image: FP2,
+        category: "Application"
+    },
+    {
+        id: 11,
+        image: FP3,
+        category: "Branding"
+    },
+    {
+        id: 12,
+        image: FP4,
+        category: "UI/UX"
+    },
+]
+
+export default Projects;
--- pages/work.jsx
+++ pages/work.jsx
@@ -1,88 +1,91 @@
+import { useState } from 'react';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
 // head props
 import Meta from '@/components/Meta';
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from "swiper";
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-
-// counterup
-import CountUp from 'react-countup';
-
-// icons
-import { CgArrowLongRight } from 'react-icons/cg';
-
-// import images
-import TextBottomImg from '../public/img/banner-text-line.png';
-import RPImg1 from '../public/img/rp-1.jpg';
-import RPImg2 from '../public/img/rp-2.jpg';
-import Service1 from '../public/img/branding.png';
-import Service2 from '../public/img/design.png';
-import Service3 from '../public/img/development.png';
-import FP1 from '../public/img/fp-1.jpg';
-import FP2 from '../public/img/fp-2.jpg';
-import FP3 from '../public/img/fp-3.jpg';
-import FP4 from '../public/img/fp-4.jpg';
-import WCIocn1 from '../public/img/why-choose-icon-1.png';
-import WCIocn2 from '../public/img/why-choose-icon-2.png';
-import WCIocn3 from '../public/img/why-choose-icon-3.png';
-import User from '../public/img/user.png';
+// get static data
+import Projects from '../data/ProjectsList';
 
 export default function Home() {
+
+  // local state
+  const [projects, setProjects] = useState(Projects);
+
+  const filterProject = (projCat) => {
+    const updateItems = Projects.filter((item) => {
+      return item.category === projCat
+    });
+    setProjects(updateItems);
+  }
+
   return (
     <>
-      <Meta />
+      <Meta title={"Work | Beeo Digital"} />
 
-      {/* featured projects start */}
-      <section className='mt-20 pb-10 md:pb-24'>
+      {/* page heading area start */}
+      <section className='pt-20 md:pt-[140px] pb-10 md:pb-20'>
         <div className="max-w-6xl mx-auto px-2">
-          <h2 className='font-volkhov font-normal text-3xl md:text-6xl text-[#091B22] mb-4 md:mb-16'>Featured projects</h2>
-          <div className="flex flex-wrap -mx-2">
-            <div className="w-full md:w-2/3 ps-2 pe-2 lg:pe-7 mb-4 lg:mb-10">
-              <a href="#">
-                <Image src={FP1} alt='featured projects' className='mb-5' />
-                <p className='text-lg'>
-                  <b>Treva</b> - Feel your experience with Treva
-                </p>
-              </a>
-            </div>
-            <div className="w-full md:w-1/3 px-2 mb-4 lg:mb-10">
-              <a href="#">
-                <Image src={FP2} alt='featured projects' className='mb-5' />
-                <p className='text-lg'>
-                  <b>Radiona</b> - Etiam pulvinar tortor id blandit for the space
-                </p>
-              </a>
-            </div>
-            <div className="w-full md:w-2/3 ps-2 pe-2 lg:pe-7 mb-4 lg:mb-10">
-              <a href="#">
-                <Image src={FP3} alt='featured projects' className='mb-5' />
-                <p className='text-lg'>
-                  <b>Creativa</b> - Phasellus eu lectus venenatis convallis sapien sit amet
-                </p>
-              </a>
-            </div>
-            <div className="w-full md:w-1/3 px-2 mb-4 lg:mb-10">
-              <a href="#">
-                <Image src={FP4} alt='featured projects' className='mb-5' />
-                <p className='text-lg'>
-                  <b>Crisp</b> - Tellus, in consequat enim hasellus eu lectus venenatis
-                </p>
-              </a>
-            </div>
-          </div>
-          <p className="text-center">
-            <a href="#" className='py-2 px-8 font-medium text-lg text-[#091B22] rounded-full inline-block border border-[#E6E8E9] hover:bg-black hover:text-white'>View all projects</a>
-          </p>
+          <p className='text-xl text-[#091B22] mb-2 md:mb-4'>Work</p>
+          <h1 className='font-volkhov text-5xl lg:text-7xl lg:leading-[90px] text-[#091B22]'>Our projects to show you <br /> what's up</h1>
         </div>
       </section>
-      {/* featured projects end */}\
+      {/* page heading area end */}
+
+      {/* project list start */}
+      <section className=''>
+        <div className="max-w-6xl mx-auto px-2">
+          {/* filter links */}
+          <ul className='flex gap-4 mb-4'>
+            <li>
+              <button onClick={() => filterProject("")}>All Projects</button>
+            </li>
+            <li>
+              <button onClick={() => filterProject("Website")}>Websites</button>
+            </li>
+            <li>
+              <button onClick={() => filterProject("Application")}>Applications</button>
+            </li>
+            <li>
+              <button onClick={() => filterProject("Branding")}>Branding</button>
+            </li>
+            <li>
+              <button onClick={() => filterProject("UI/UX")}>UI/UX</button>
+            </li>
+          </ul>
+
+          <div className="flex flex-wrap -mx-2">
+
+            {
+              projects.map((item) => {
+                return item.id % 2 == 0 ? (
+                  <div className="w-full md:w-1/3 px-2 mb-4 lg:mb-10" key={item.id}>
+                    <Link href="#">
+                      <Image src={item.image} alt='projects' className='mb-5 hover:shadow-lg hover:shadow-gray-500' />
+                      <p className='text-lg'>
+                        <b>Radiona</b> - Etiam pulvinar tortor id blandit for the space
+                      </p>
+                    </Link>
+                  </div>
+                ) : (
+                  <div className="w-full md:w-2/3 ps-2 pe-2 lg:pe-7 mb-4 lg:mb-10" key={item.id}>
+                    <Link href="#">
+                      <Image src={item.image} alt='projects' className='mb-5 hover:shadow-lg hover:shadow-gray-500' />
+                      <p className='text-lg'>
+                        <b>Treva</b> - Feel your experience with Treva
+                      </p>
+                    </Link>
+                  </div>
+                )
+              })
+            }
+
+          </div>
+        </div>
+      </section>
+      {/* project list end */}
 
     </>
   )
