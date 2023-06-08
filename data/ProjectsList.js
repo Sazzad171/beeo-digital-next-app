57918cd9ef3b5b0082b63ee4cf71e12cab708e55
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-06-08 12:49:00 +0600

    update

--- data/ProjectsList.js
+++ data/ProjectsList.js
@@ -14,57 +14,57 @@ const Projects = [
         id: 1,
         image: FP1,
         category: "Website",
-        name: '',
-        details: ''
+        name: 'TopDoc',
+        details: "Improving the doctor appointment booking app's"
     },
     {
         id: 2,
         image: FP2,
         category: "Application",
-        name: '',
-        details: ''
+        name: 'Radiona',
+        details: 'Etiam pulvinar tortor id blandit for the space'
     },
     {
         id: 3,
         image: FP3,
         category: "Branding",
-        name: '',
-        details: ''
+        name: 'BestStay',
+        details: 'UI/UX case study'
     },
     {
         id: 4,
         image: FP4,
         category: "UI/UX",
-        name: '',
-        details: ''
+        name: 'Crisp',
+        details: 'Tellus, in consequat enim hasellus eu lectus venenatis'
     },
     {
         id: 5,
         image: FP5,
         category: "Website",
-        name: '',
-        details: ''
+        name: 'Pondith',
+        details: 'A Gamified E-Learning Case Study'
     },
     {
         id: 6,
         image: FP6,
         category: "Application",
-        name: '',
-        details: ''
+        name: 'APPZIO',
+        details: 'Tellus, in consequat enim hasellus eu lectus venenatis'
     },
     {
         id: 7,
         image: FP7,
         category: "Branding",
-        name: '',
-        details: ''
+        name: 'RentUncle',
+        details: 'Phasellus eu lectus venenatis convallis sapien sit amet'
     },
     {
         id: 8,
         image: FP8,
         category: "UI/UX",
-        name: '',
-        details: ''
+        name: 'RedDwarf',
+        details: 'Tellus, in consequat enim hasellus eu lectus venenatis'
     },
 ]
 
